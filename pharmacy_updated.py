655c8a133ee7206d8feed80154f8305c8d987562
Manideep0704 <manideepreddy2477@gmail.com>
2026-05-21 16:59:31 +0530

    Update pharmacy_updated.py

--- pharmacy_updated.py
+++ pharmacy_updated.py
@@ -14,6 +14,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+pip install groq python-dotenv
 from dotenv import load_dotenv
 
 # # This searches for the .env file and loads the variables
